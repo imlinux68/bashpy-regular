c8cc5dba2cb7f7160125b6220a5c86350f26f7c9
imlinux68 <imlinux68@gmail.com>
2023-06-27 07:25:20 +0300

    complete first mission

--- 1.square.py
+++ 1.square.py
@@ -1,8 +1,9 @@
 #!/bin/python
 
 def sqfunc():
+    num = int(input("Enter a number to square it: "))
     print("number \t\tsquare")
-    for x in range(1,(15+1)):
+    for x in range(1, ( num + 1 )):
         print(str(x)+" \t\t"+str(x*x))
 
 sqfunc()
