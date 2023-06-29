6ff91310e09c08a0786320b86bf5931fdd4c8b3a
imlinux68 <imlinux68@gmail.com>
2023-06-29 23:53:55 +0300

    added bash script

--- 2.circeRadius.sh
+++ 2.circeRadius.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+function area(){
+    read -rp "Enter a radius: " radius
+    local area_of_circle=$( bc <<< 3.14*$radius*$radius )
+    echo "The area of circe is equal to: $area_of_circle"
+}
+
+area
