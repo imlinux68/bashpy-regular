75b5ae6d55f6a584155dfdb9f0941f84885aa0ab
imlinux68 <imlinux68@gmail.com>
2023-06-29 23:48:18 +0300

    improved python script output

--- 2.circeRadius.py
+++ 2.circeRadius.py
@@ -4,6 +4,6 @@
 def area():
     radius = float(input("Enter Radius: "))
     area_of_circle = 3.14*radius*radius
-    print(area_of_circle)
+    print("The area of circe is equal to " + str(area_of_circle))
 
 area()
