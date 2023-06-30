5018ee8efffdaf4160786d56e52af1b2527cca7b
imlinux68 <imlinux68@gmail.com>
2023-06-30 20:11:28 +0300

    completed python 4

--- 4.voting.py
+++ 4.voting.py
@@ -7,7 +7,7 @@ def voting():
     if(age>=18):
         print("Hi, " + name + " You can take part in voting!")
     else:
-        print("Unfortunately, " + name + " you are under 18 :( ")
+        print("Unfortunately, " + name + " you are under 18 :( \nDear, " + name + " You will be able to vote after " + str(18-age) + " years" )
 
 
 voting()
