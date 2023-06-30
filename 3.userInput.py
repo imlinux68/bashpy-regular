6a8fab5dca6ed13fa1206369360a3bbe7cb675fe
imlinux68 <imlinux68@gmail.com>
2023-06-30 10:07:03 +0300

    entered python script

---
+++ 3.userInput.py
@@ -0,0 +1,9 @@
+#!/bin/python
+
+def nameAge():
+    name = input("Enter your name please: ")
+    age = int(input("Enter your age please: "))
+    print("Hello! " + name + ", You are " + str(age) + " years old!")
+
+
+nameAge()
