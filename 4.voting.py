98589ef33ae3432ac045a7aeee752882b3a8b410
imlinux68 <imlinux68@gmail.com>
2023-06-30 20:17:17 +0300

    added 4 bash

--- 4.voting.py
+++ 4.voting.py
@@ -5,9 +5,9 @@ def voting():
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
     if(age>=18):
-        print("Hi, " + name + " You can take part in voting!")
+        print("Hi " + name + ", You can take part in voting!")
     else:
-        print("Unfortunately, " + name + " you are under 18 :( \nDear, " + name + " You will be able to vote after " + str(18-age) + " years" )
+        print("Unfortunately " + name + ", you are under 18 :( \nDear " + name + ", You will be able to vote after " + str(18-age) + " years" )
 
 
 voting()
--- 4.voting.sh
+++ 4.voting.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 function voting(){
-
-
+    read -rp "Enter your name: " name
+    read -rp "Enter your age: " age
+    if [[ $age -lt 18 ]]
+        then 
+            echo -e "Unfortunately $name, you are under 18 :( \nDear $name, You will be able to vote after $(( 18 - age )) years"
+        else 
+            echo -e "Dear $name, you are able to vote!"
+    fi
 }
 
 voting
