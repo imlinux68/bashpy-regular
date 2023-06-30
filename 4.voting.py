433f15ce950f2ce94fb39b0dc916a6339a386b0d
imlinux68 <imlinux68@gmail.com>
2023-06-30 20:09:19 +0300

    finished python4

--- 4.voting.py
+++ 4.voting.py
@@ -4,6 +4,10 @@
 def voting():
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
-    
+    if(age>=18):
+        print("Hi, " + name + " You can take part in voting!")
+    else:
+        print("Unfortunately, " + name + " you are under 18 :( ")
+
 
 voting()
