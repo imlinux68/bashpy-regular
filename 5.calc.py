c1dfbc9a640cec0b66d641276f0139ec91304312
imlinux68 <imlinux68@gmail.com>
2023-07-03 17:14:23 -0600

    added add python func

--- 5.calc.py
+++ 5.calc.py
@@ -3,6 +3,6 @@
 def calculate():
     firstNum = int(input("Enter first number: "))
     secondNum = int(input("Enter second number: "))
-
+    print("Additional of two numbers is: " + str(firstNum+secondNum))
     
 calculate()
