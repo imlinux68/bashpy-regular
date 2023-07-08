e2055323ee2f9772e04024b816b521fa5c81204e
imlinux68 <imlinux68@gmail.com>
2023-07-08 14:12:59 -0600

    finished python

--- 5.calc.py
+++ 5.calc.py
@@ -4,5 +4,9 @@ def calculate():
     firstNum = int(input("Enter first number: "))
     secondNum = int(input("Enter second number: "))
     print("Additional of two numbers is: " + str(firstNum+secondNum))
-    
+    print("Substract of two numbers is: " + str(firstNum-secondNum))
+    print("Division of two numbers is: " + str(firstNum/secondNum))
+    print("Multiply of two numbers is: " + str(firstNum*secondNum))
+    print("Module of two numbers is: " + str(firstNum%secondNum))
+
 calculate()
