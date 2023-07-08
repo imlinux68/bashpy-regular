35a725c7559c5a8b43b2f65e0a61536a9dae92b5
imlinux68 <imlinux68@gmail.com>
2023-07-08 14:18:02 -0600

    added add bash func

--- 5.calc.sh
+++ 5.calc.sh
@@ -3,9 +3,11 @@
 function calculate(){
     read -rp "Enter a first num: " fnum
     read -rp "Enter a second num: " snum
-
+    echo "Addition of two numbers is: " $(( fnum + snum ))
 
 
 
     
 }
+
+calculate
