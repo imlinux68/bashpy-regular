57d0011b6918feaeb6cc6923dea5a5b7bdd16710
imlinux68 <imlinux68@gmail.com>
2023-07-11 23:47:51 +0300

    finished bash func

--- 5.calc.sh
+++ 5.calc.sh
@@ -4,7 +4,12 @@ function calculate(){
     read -rp "Enter a first num: " fnum
     read -rp "Enter a second num: " snum
     echo "Addition of two numbers is: " $(( fnum + snum ))
-
+    echo "Substruction of two numbers is: " $(( fnum - snum ))
+    echo -ne "Division of two numbers is: " 
+    echo "scale=4; $fnum / $snum" | bc
+    echo "Multiply of two numbers is: " $(( fnum * snum ))
+    echo "Modulus of two numbers is: " $(( fnum % snum ))
+    
 
 
     
