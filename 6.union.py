2bd8a9b7981d735ba225e78ac94c580e14a5facb
imlinux68 <imlinux68@gmail.com>
2023-07-14 14:52:12 -0600

    added union py

--- 6.union.py
+++ 6.union.py
@@ -1,7 +1,9 @@
 #/bin/python
 
 def union():
-
-
-
+    print("Now lets find an union of 2 arrays")
+    arrayA = {3,5,12,31,65}
+    arrayB = {6,5,2,1}
+    union_array = arrayA.union(arrayB)
+    print(union_array)
 union()
