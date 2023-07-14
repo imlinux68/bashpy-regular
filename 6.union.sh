704f3540a7119ebe7c97b55adc69c569c8758b94
imlinux68 <imlinux68@gmail.com>
2023-07-14 14:42:12 -0600

    added union bash

--- 6.union.sh
+++ 6.union.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 function union() {
-
-
-
-
+    echo "Lets find union of two arrays"
+    arrayA=(3 4 5 23 45 109)
+    arrayB=(3 4 7 31 45 54)
+    union_array=($(echo "${arrayA[@]}" "${arrayB[@]}" | tr ' ' '\n' | sort -un | tr '\n' ' '))
+    echo "${union_array[@]}"
 }
 
 union
