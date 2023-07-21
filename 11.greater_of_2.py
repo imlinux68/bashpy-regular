835a7ab128a9c6407ddd4c248244ab49219f9d44
imlinux68 <imlinux68@gmail.com>
2023-07-21 14:59:08 -0600

    finished py max

--- 11.greater_of_2.py
+++ 11.greater_of_2.py
@@ -1,6 +1,12 @@
 #!/bin/python
 
 def compare2():
+    print("Finding maximum number")
+    num1 = int(input("Enter 1 num: "))
+    num2 = int(input("Enter 2 num: "))
 
-
+    if(num1>num2):
+        print("First number is a greater! that is: " + str(num1))
+    else:
+        print("Second number is a greater! that is: " + str(num2))
 compare2()
