06cbd7295361a11d7c7dacd21661b11ad369c81e
imlinux68 <imlinux68@gmail.com>
2023-07-21 14:12:08 -0600

    finishe bash

--- 11.greater_of_2.sh
+++ 11.greater_of_2.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 function compare2(){
+    read -p "Enter first num: " num1
+    read -p "Enter second num: " num2
 
-}
+    if [[ $num1 -gt $num2 ]]
+        then
+            echo "First number that is $num1 is greater than second number that is $num2"
+        else
+            echo "Second number that is $num2 is greater than first number that is $num1"
+    fi
+ }
 
 compare2
