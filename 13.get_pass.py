7c8e5b3465507cae78cd4e72333d3ea05ead5c73
imlinux68 <imlinux68@gmail.com>
2023-07-23 18:58:08 -0600

    added new file py

--- 13.get_pass.py
+++ 13.get_pass.py
@@ -1,7 +1,11 @@
 #!/bin/python
 
 def get_pass():
-    
+    user_password = input("Enter your password: ")
+    if(user_password.isalnum()):
+        print("Your password is okey")
+    else:
+        print("Your password is not an alphanumeric or have spaces")
 
 
 
