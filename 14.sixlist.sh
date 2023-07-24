185503cb3633fbaef25b294c5697d7eec90a0b87
imlinux68 <imlinux68@gmail.com>
2023-07-24 09:37:13 -0600

    finished bash

--- 14.sixlist.sh
+++ 14.sixlist.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 function sixlist(){
-    
+    lst=()
+    for ((i=1; i<=6; i++))
+        do
+            read -p "Enter a sum to array: " sum_array
+            arr+=("$sum_array")
+        done
+    echo "this is yur array: ${arr[@]}"
+
+    s=0
+    for i in ${lst[@]}
+        do
+            s=s+i
+        done
+    echo "Sum of array elements are: $s"
 }
+
+sixlist
