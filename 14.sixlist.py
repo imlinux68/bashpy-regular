2f859a3849a5c094f4d24ea933fc096cd9e26983
imlinux68 <imlinux68@gmail.com>
2023-07-24 09:31:08 -0600

    finished python

--- 14.sixlist.py
+++ 14.sixlist.py
@@ -1,7 +1,15 @@
 #!/bin/python
 
 def sixlist():
+    lst = []
+    for i in range(6):
+        lst.append(int(input("Enter 6 nums: ")))
+    print("This is the list")
+    print(lst)
 
-
+    s = 0
+    for i in lst:
+        s += i
+    print("This is the sum: " + str(s))    
 
 sixlist()
