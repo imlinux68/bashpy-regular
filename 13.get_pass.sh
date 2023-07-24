544d493459f8cac19e94a7ec4c7d0f67489cadba
imlinux68 <imlinux68@gmail.com>
2023-07-23 19:12:08 -0600

    finiished bash script

--- 13.get_pass.sh
+++ 13.get_pass.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 function get_pass(){
+    read -sp "Enter your password here: " user_password
+    if ! [[ "$user_password" =~ [:alnum:]+ ]]
+        then
+            echo -e "\nYour password is not a valid alphanumeric or has a spaces"
+        else
+            echo -e "\nyour password is Okay!"
+    fi
 
 }
 
