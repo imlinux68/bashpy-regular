c84485d0704e4fa32963a2a53ec306968185bfe5
imlinux68 <imlinux68@gmail.com>
2023-07-26 10:35:08 -0600

    finished bash

--- 16.statement.sh
+++ 16.statement.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-function(){
-    
+function statements(){
+	read -p "Enter a num: " num
+	echo "added num: $((num += 5))"
+	echo "substructed num $((num -=5))"
+	echo "multiplied num $((num *=5 ))"
+	echo "divided num $((num /=5))"
 }
+
+statements
+
