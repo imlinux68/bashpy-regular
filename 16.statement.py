d156a843f4b26db75a201714d19529cab8b28a98
imlinux68 <imlinux68@gmail.com>
2023-07-26 10:18:08 -0600

    finished python

--- 16.statement.py
+++ 16.statement.py
@@ -1,5 +1,14 @@
 #!/bin/python
 
-def func():
+def statements():
+	num = int(input("Enter an integer: "))
+	num += 5
+	print("added num: " + str(num))
+	num -= 5
+	print("substructed num: " + str(num))
+	num *= 5
+	print("multiplied num: " + str(num))
+	num /= 5
+	print("divided num: " + str(num))
 
-
+statements()
