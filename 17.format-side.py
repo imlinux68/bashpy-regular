a5907999a92965ae1c19f628ecd463639d58f5de
imlinux68 <imlinux68@gmail.com>
2023-07-28 12:12:59 -0600

    added python 17 script

---
+++ 17.format-side.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+def side_piramyd():
+    print("A")
+    print("B\tC")
+    print("D\tE\tF")
+    print("G\tH\tI\tJ")
+    print("K\tL\tM")
+    print("N\tO")
+    print("P")
+
+
+
+side_piramyd()
