d214cc74d2427bc25d71de5714bd0e9216ffc5ff
imlinux68 <imlinux68@gmail.com>
2023-07-29 22:12:59 -0600

    added 18 py

---
+++ 18.uid.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+def dict_uid():
+    user_info={"name":"Bob","address":"TV","contact":"384327025"}
+    print(user_info)
+    user_info.update({"contact":"122142141"})
+    print(user_info)
+
+dict_uid()    
+
+
